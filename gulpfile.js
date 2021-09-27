a0ae2e99febe8227a915923eb339692517e7dced
Rafael Rodrigues <rafael.raraujo86@gmail.com>
2021-09-27 20:02:03 +0200

    Changed back from docs to dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@ const sass = require('gulp-sass');
 const prefix = require('gulp-autoprefixer');
 
 function copyHTML() {
-  return src('./src/index.html').pipe(dest('./docs'));
+  return src('./src/index.html').pipe(dest('./dist'));
 }
 
 function copyJS() {
-  return src('./src/js/modal.js').pipe(dest('./docs/js'));
+  return src('./src/js/modal.js').pipe(dest('./dist/js'));
 }
 
 function compilecss() {
   return src('./src/scss/**/*.scss')
     .pipe(sass())
     .pipe(prefix())
-    .pipe(dest('./docs/css'));
+    .pipe(dest('./dist/css'));
 }
 
 function watchTask() {
